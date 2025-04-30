300db8cdfcf29925a9a944b769070a5fca080c54
liluppis <lisa.upp@hotmail.com>
2025-04-30 15:49:00 +0200

    Initial commit - Anna, Lisa

---
+++ src/main/java/com/annalisa/cardgame/Main.java
@@ -0,0 +1,12 @@
+package com.annalisa.cardgame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Let's build a card game!");
+
+    }
+
+}
