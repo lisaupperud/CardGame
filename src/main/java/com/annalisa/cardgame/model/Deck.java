ec809493c870244fe48fe2d92dff08452ee5dff4
liluppis <lisa.upp@hotmail.com>
2025-05-06 14:34:49 +0200

    Refactor: shuffledDeck() and printDeck() in Deck class - D: Lisa, N: Anna

--- src/main/java/com/annalisa/cardgame/model/Deck.java
+++ src/main/java/com/annalisa/cardgame/model/Deck.java
@@ -1,6 +1,7 @@
 package com.annalisa.cardgame.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Deck {
@@ -10,15 +11,6 @@ public class Deck {
     public static void generateDeck() {
         deck = new ArrayList<>();
         for (int i = 1; i <= 13; i++) {
-            /*deck.add(i, new Card(0, i));
-            //System.out.println(deck[i].getSuitAsString(0) + " " + deck[i].getValueAsString(i));
-            deck.add(i + 13, new Card(1, i));
-            //System.out.println(deck[i].getSuitAsString(1) + " " + deck[i].getValueAsString(i));
-            deck.add(i + 26, new Card(2, i));
-            //System.out.println(deck[i].getSuitAsString(2) + " " + deck[i].getValueAsString(i));
-            deck.add(i + 39, new Card(3, i));
-            //System.out.println(deck[i].getSuitAsString(3) + " " + deck[i].getValueAsString(i));*/
-
             deck.add(new Card(0, i));
             deck.add(new Card(1, i));
             deck.add(new Card(2, i));
@@ -27,22 +19,10 @@ public class Deck {
     }
 
     public static void shuffleDeck() {
-        generateDeck();
-        Random randomNumber = new Random();
-
-        Card temp;
-        for (int card = 1; card <= 51; card++) {
-            // Find a random place in the deck
-            int rand = randomNumber.nextInt(51) + 1;
-            // Swap cards in deck
-            temp = deck.get(card);
-            deck.set(card, deck.get(rand));
-            deck.set(rand, temp);
-        }
+        Collections.shuffle(deck, new Random());
     }
 
     public static void printDeck() {
-        generateDeck();
         for (Card card : deck) {
             System.out.println(card.getValueAsString(card.getValue()) + " of " + card.getSuitAsString(card.getSuit()));
         }
