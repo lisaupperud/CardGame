f2de8f27fac3512392a9cd4003a1e6382d80504a
liluppis <lisa.upp@hotmail.com>
2025-05-15 13:32:02 +0200

    Add: comparePointsTest() to GamePlayTest class - D: Lisa, N: Anna

--- src/test/java/com/annalisa/cardgame/service/GamePlayTest.java
+++ src/test/java/com/annalisa/cardgame/service/GamePlayTest.java
@@ -18,6 +18,9 @@ public class GamePlayTest {
     Player player1;
     Player player2;
     Deck cardDeck;
+    Hand handObject;
+    ArrayList<Card> testPointPile;
+    ArrayList<Card> testPointPile2;
 
     @BeforeEach
     @DisplayName("Generate test deck before each test")
@@ -33,5 +36,31 @@ public class GamePlayTest {
         player1 = new Player("A");
         player2 = new Player("B");
         assertNotSame(player1.hand.handArray, player2.hand.handArray);
+    }
+
+    @Test
+    @DisplayName("Compares pointsPiles to determine winner")
+    void comparePointsTest() {
+        player1 = new Player("A");
+        player2 = new Player("B");
+        testPointPile = player1.getHand().pointPile;
+        testPointPile2 = player2.getHand().pointPile;
+        boolean gameOver;
+
+        testPointPile.addAll(cardDeck.deck);
+
+
+        if (testPointPile.size() >= 28) {
+            gameOver = true;
+            System.out.println("Player1 won!");
+        } else if (testPointPile2.size() == 28) {
+            gameOver = true;
+            System.out.println("Player2 won!");
+        } else {
+            gameOver = false;
+        }
+
+        assertTrue(gameOver);
+
     }
 }
